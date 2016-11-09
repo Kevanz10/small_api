693d55a787a1130251fc1cb3566c9961ced18dae
kevanz10 <krojas1171@gmail.com>
2016-11-09 08:11:20 -0500

    method for destroying model's associations

--- app/models/universe.rb
+++ app/models/universe.rb
@@ -1,8 +1,18 @@
 class Universe < ApplicationRecord
-	has_many :heroes, :dependent => :delete
+	before_destroy :destroy_heroes
+	has_many :heroes, :class_name => "Heroe", dependent: :destroy
+
 	#this method will overwrite to_json to avoid code repetition.
 	def as_json(options)
    options[:except] ||= [:created_at, :updated_at]
    super(options)
  	end
+
+ 	#method for deleting all associated models
+
+   private
+
+   def destroy_heroes
+     self.heroes.delete_all   
+   end
 end
